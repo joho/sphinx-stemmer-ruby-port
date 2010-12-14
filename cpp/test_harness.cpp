4068cee9a4196b0e81d8654c8e08b2a3d713beae
John Barton (joho) <jrbarton@gmail.com>
2010-12-14 19:55:12 +1100

    got my test harness reading in all the words in the dictionary. wow C is a lot, lot faster than ruby :)
    except it took for bloody forever to write this

--- cpp/test_harness.cpp
+++ cpp/test_harness.cpp
@@ -4,8 +4,26 @@
 
 int main(int argc, const char* argv[])
 {
+  int total_number_of_words, max_length_of_any_word;
+
   FILE *scrabble_word_list_file;
+
   scrabble_word_list_file = fopen("words/TWL06.txt", "r");
+  
+  // i know these cos i cheated and measured them
+  // using irb. god files and string are a PITA in C
+  total_number_of_words = 178691;
+  max_length_of_any_word = 15;
+
+  char words[total_number_of_words][max_length_of_any_word + 3];
+  for(int i = 0; i < total_number_of_words; i++) 
+  {
+    fgets(words[i], max_length_of_any_word + 3, scrabble_word_list_file);
+  }
+
+  for(int i = 100; i < 200; i++)
+    printf("%s", words[i]);
+
 
   stem_en_init();
 
