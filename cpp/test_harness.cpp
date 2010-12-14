a14c3e9ddbdeab59cf300e046ccf1d27a762f80c
John Barton (joho) <jrbarton@gmail.com>
2010-12-14 19:05:56 +1100

    get the test harness compiling again. god i can barely remember c or c++

--- cpp/test_harness.cpp
+++ cpp/test_harness.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, const char* argv[])
 {
-  int stem_return_code = stem_en_init();
-  printf("yes you have compiled correctly\n%d\n", stem_return_code);
+  stem_en_init();
+  printf("yes you have compiled correctly\n");
 }
