75da222f22589fdfed7dacda4c677ebc72ada106
John Barton (joho) <jrbarton@gmail.com>
2010-12-14 21:12:11 +1100

    WIP on getting this thing to stem the whole scrabble dictionary.

--- cpp/test_harness.cpp
+++ cpp/test_harness.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 
 #include "sphinxstem.h"
 
@@ -17,16 +18,21 @@ int main(int argc, const char* argv[]) {
   // FYI BYTE = typedef unsigned char
   BYTE words[total_number_of_words][max_length_of_any_word + 3];
   int current_word = 0;
+  int current_char = 0;
   while(current_word < total_number_of_words) {
-    int current_char = 0;
-    if ((words[current_word][current_char] = fgetc(scrabble_word_list_file)) == (BYTE)EOF) {
+    if ((words[current_word][current_char] = (BYTE)fgetc(scrabble_word_list_file)) == (BYTE)EOF) {
+      printf("reached end of file");
       break;
     }
     else {
+      // printf("%c", words[current_word][current_char]);
       if (words[current_word][current_char] == '\n') {
+        words[current_word][current_char - 1] = '\0';
         current_word++;
-      }
-      else {
+        current_char = 0;
+
+      } else {
+        words[current_word][current_char] = tolower(words[current_word][current_char]);
         current_char++;
       }
     }
@@ -36,8 +42,13 @@ int main(int argc, const char* argv[]) {
 
   for(int i = 0; i < total_number_of_words; i++)
   {
-    stem_en(words[i]);
-    printf("%s", words[i]);
+    // printf("%s", words[i]);
+    BYTE stemmed[15];
+    strcpy((char*)stemmed, (char*)words[i]);
+    stem_en(stemmed);
+    if (strcmp((char*)words[i], (char*)stemmed)) {
+      printf("%s %s\n", words[i], stemmed);
+    }
   }
   // to test
   // stem_en( BYTE * pWord);
