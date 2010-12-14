7b029b9f0b50bf895e256e054852f6e08a404baa
John Barton (joho) <jrbarton@gmail.com>
2010-12-14 19:08:53 +1100

    mark out what i want to test

--- cpp/test_harness.cpp
+++ cpp/test_harness.cpp
@@ -5,5 +5,11 @@
 int main(int argc, const char* argv[])
 {
   stem_en_init();
+
+  // to test
+  // stem_en( BYTE * pWord);
+  // stem_soundex( BYTE * pWord);
+  // stem_dmetaphone( BYTE * pWord, bool bUTF8);
+
   printf("yes you have compiled correctly\n");
 }
